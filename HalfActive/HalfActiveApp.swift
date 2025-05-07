424bb07d6d24e86b83e9e6293f3bfc03a2efd12f
Siddharth <siddharth952@gmail.com>
2025-05-07 16:21:19 +0530

    Initial Commit

---
+++ HalfActive/HalfActiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  HalfActiveApp.swift
+//  HalfActive
+//
+//  Created by Siddharth S on 07/05/25.
+//
+
+import SwiftUI
+
+@main
+struct HalfActiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
